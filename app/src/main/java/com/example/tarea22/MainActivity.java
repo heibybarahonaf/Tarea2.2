8a7f60c38f8e0ee7d04f549e8bedbf299cecfa90
Heiby <heibybarahona@gmail.com>
2023-11-10 22:00:06 -0600

    Actualización

--- app/src/main/java/com/example/tarea22/MainActivity.java
+++ app/src/main/java/com/example/tarea22/MainActivity.java
@@ -2,15 +2,40 @@ package com.example.tarea22;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.annotation.SuppressLint;
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
 
+    private Button btnList, btnConsul;
+
+    @SuppressLint("MissingInflatedId")
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        btnList = findViewById(R.id.btnList);
+        btnConsul = findViewById(R.id.btnConsulta);
+
+        btnList.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(getApplicationContext(),ActivitListView.class);
+                startActivity(intent);
+            }
+        });
+
+        btnConsul.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(getApplicationContext(),ActivityConsulta.class);
+                startActivity(intent);
+            }
+        });
 
     }
 }
